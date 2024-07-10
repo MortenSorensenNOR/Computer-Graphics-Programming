45204ca83999ab70b11c9585b5bfe5f65b2ad2c7
Morten Sørensen <morten.sorensen.04@gmail.com>
2024-07-10 21:47:33 +0200

    some light planning

--- software_rendering/main.cpp
+++ software_rendering/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char** argv) {
     srand(time(0));
 
     Engine_t engine;
-    int err = engine_init(&engine, 1920, 1080, "Computer Graphics Programming", M_PI / 4);
+    int err = engine_init(&engine, 1920, 1080, "Computer Graphics Programming", 90.0f);
     if (err != 0) {
         return 0;
     }
